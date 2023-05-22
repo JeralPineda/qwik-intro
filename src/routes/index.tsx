c13e6c3c6042a054db1dcf36d11d1ac1980ff231
Jeral Pineda <jeral.cris@gmail.com>
2023-05-21 18:28:58 -0600

    feat: Mantener estado - Signals

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,29 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const pokemonId = useSignal(1); //Primitivos boolean,string, etc
+
   return (
     <>
-      <span class="text-5xl">Hola mundo</span>
+      <span class="text-2xl">Buscador simple</span>
+      <span class="text-9xl">{pokemonId}</span>
+
+      {/* TODO: Crear imagen */}
+      <div class="margin-top-2">
+        <button class="btn btn-primary mr-2">Anterior</button>
+        <button class="btn btn-primary">Anterior</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'PokeQwik',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Esta es mi primer app en qwik',
     },
   ],
 };
